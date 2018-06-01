0d0f61984a34e3b3fb0088df5b4a3a9a0577fb6e
wb <846022604@qq.com>
2018-06-01 11:07:53 +0800

    mybatis Generator配置

---
+++ src/main/java/com/example/libarymanage/dao/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.libarymanage.dao.mapper;
+
+import com.example.libarymanage.entity.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
--- src/main/java/com/example/libarymanage/entity/User.java
+++ src/main/java/com/example/libarymanage/entity/User.java
@@ -2,10 +2,12 @@ package com.example.libarymanage.entity;
 
 public class User {
     private Integer id;
+
     private String name;
+
     private String password;
 
-    public User(Integer id, String name, String password){
+    public User(Integer id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
@@ -14,22 +16,28 @@ public class User {
     public User() {
         super();
     }
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
-        this.name = name;
+        this.name = name == null ? null : name.trim();
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
-        this.password = password;
+        this.password = password == null ? null : password.trim();
     }
 }
