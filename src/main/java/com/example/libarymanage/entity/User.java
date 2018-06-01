33c0989df5caa6747b9b30692147f0fe45ed22f3
wb <846022604@qq.com>
2018-06-01 09:15:50 +0800

    初始化后台成功整合mybatis

--- src/main/java/com/example/libarymanage/entity/User.java
+++ src/main/java/com/example/libarymanage/entity/User.java
@@ -1,14 +1,23 @@
 package com.example.libarymanage.entity;
 
 public class User {
-    private String id;
+    private Integer id;
     private String name;
     private String password;
 
-    public String getId() {
+    public User(Integer id, String name, String password){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+        super();
+    }
+    public Integer getId() {
         return id;
     }
-    public void setId(String id) {
+    public void setId(Integer id) {
         this.id = id;
     }
     public String getName() {
